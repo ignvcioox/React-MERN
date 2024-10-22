382b95bd95b0a6ed0fcd58d3e50f01a0b779a5f4
Benjamín Ignacio López Durán <benjaminigna72@gmail.com>
2024-10-21 22:05:11 -0300

    React App Ready

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
@@ -12,6 +13,10 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
